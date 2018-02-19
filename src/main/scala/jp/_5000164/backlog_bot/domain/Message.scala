320d16f765a20a1db6f384c60f350709ab0db1f6
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-02-20 08:44:38 +0900

    refactor: 投稿するメッセージを構築する処理を抜き出した

--- src/main/scala/jp/_5000164/backlog_bot/domain/Message.scala
+++ src/main/scala/jp/_5000164/backlog_bot/domain/Message.scala
@@ -2,7 +2,6 @@ package jp._5000164.backlog_bot.domain
 
 case class Message(
                     title: String,
-                    content: String,
-                    updatedUser: String,
-                    link: String
+                    link: String,
+                    content: String
                   )
