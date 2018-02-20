92b335d9af19dbc2b287919fb16041c15e4b14f5
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-02-21 08:59:19 +0900

    feat: 情報を見やすくするためにタイトルに情報を集める

--- src/main/scala/jp/_5000164/backlog_bot/domain/BuildMessage.scala
+++ src/main/scala/jp/_5000164/backlog_bot/domain/BuildMessage.scala
@@ -1,13 +1,15 @@
 package jp._5000164.backlog_bot.domain
 
 object BuildMessage {
-  def commentTitle(title: String): String = title
+  def commentTitle(title: String, projectKey: String, issueId: Long, updatedUser: String, createdAt: java.util.Date): String = {
+    s"[$projectKey-$issueId] $title へコメント追加 $updatedUser ${"%tF %<tT" format createdAt}"
+  }
 
   def commentLink(spaceId: String, projectKey: String, issueId: Long, commentId: Long): String = {
     s"https://$spaceId.backlog.jp/view/$projectKey-$issueId#comment-$commentId"
   }
 
-  def commentText(content: String, updatedUser: String, createdAt: java.util.Date): String = {
-    s"${content.take(7900)}\n\ncreated by $updatedUser\ncreated at ${"%tF %<tT" format createdAt}"
+  def commentText(content: String): String = {
+    s"${content.take(8000)}"
   }
 }
