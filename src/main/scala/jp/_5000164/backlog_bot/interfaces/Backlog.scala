8ecf8d7ea4d27bc3e16ef694c0c38e02d20895af
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-02-25 23:22:28 +0900

    feat: 課題の追加に対応

--- src/main/scala/jp/_5000164/backlog_bot/interfaces/Backlog.scala
+++ src/main/scala/jp/_5000164/backlog_bot/interfaces/Backlog.scala
@@ -1,7 +1,7 @@
 package jp._5000164.backlog_bot.interfaces
 
 import com.nulabinc.backlog4j.conf.{BacklogConfigure, BacklogJpConfigure}
-import com.nulabinc.backlog4j.internal.json.activities.{IssueCommentedContent, IssueUpdatedContent}
+import com.nulabinc.backlog4j.internal.json.activities.{IssueCommentedContent, IssueCreatedContent, IssueUpdatedContent}
 import com.nulabinc.backlog4j.{Activity, BacklogClient, BacklogClientFactory}
 import jp._5000164.backlog_bot.domain.{BuildMessage, Message}
 
@@ -19,7 +19,16 @@ class Backlog {
     val activities = client.getProjectActivities(project.getId)
     val activity = activities.asScala.head
 
-    if (activity.getType == Activity.Type.IssueUpdated) {
+    if (activity.getType == Activity.Type.IssueCreated) {
+      val content = activity.getContent.asInstanceOf[IssueCreatedContent]
+      val issue = client.getIssue(content.getId)
+      Some(Message(
+        BuildMessage.createPretext(projectKey, content.getKeyId, activity.getCreatedUser.getName, activity.getCreated, issue.getPriority.getName, issue.getAssignee.getName),
+        BuildMessage.createTitle(content.getSummary),
+        BuildMessage.createLink(spaceId, projectKey, content.getKeyId),
+        BuildMessage.createText(content.getDescription)
+      ))
+    } else if (activity.getType == Activity.Type.IssueUpdated) {
       val content = activity.getContent.asInstanceOf[IssueUpdatedContent]
       val comment = client.getIssueComment(content.getId, content.getComment.getId)
       Some(Message(
