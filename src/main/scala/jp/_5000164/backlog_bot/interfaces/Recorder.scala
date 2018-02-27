db065d804b02c606a730a8c94f674c9320bc9e45
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-02-27 22:26:49 +0900

    feat: 最後に実行した時間をファイルに保存するようにして前回実行時からの差分だけを出力するようにする

--- src/main/scala/jp/_5000164/backlog_bot/interfaces/Recorder.scala
+++ src/main/scala/jp/_5000164/backlog_bot/interfaces/Recorder.scala
@@ -1,8 +1,21 @@
 package jp._5000164.backlog_bot.interfaces
 
+import java.io.PrintWriter
+import java.nio.file.{Files, Paths}
 import java.text.SimpleDateFormat
 import java.util.Date
 
+import scala.io.Source
+
 object Recorder {
-  def getLastExec: Date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2018-02-26 00:00:00")
+  def getLastExec: Date =
+    if (Files.notExists(Paths.get(".record"))) new Date
+    else new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(Source.fromFile(".record").mkString)
+
+  def record(executedAt: Date): Unit = {
+    if (Files.notExists(Paths.get(".record"))) Files.createFile(Paths.get(".record"))
+    val pw = new PrintWriter(".record")
+    pw.write("%tF %<tT" format executedAt)
+    pw.close()
+  }
 }
