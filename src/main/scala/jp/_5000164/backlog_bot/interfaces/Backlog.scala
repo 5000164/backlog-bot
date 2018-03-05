12557ce1153be75a4455e68d04f9b61a1bab4ac6
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-03-05 22:01:10 +0900

    refactor: 処理を共通化するために似たような処理をまとめられる箇所に移譲
    一度 domain に処理を委譲してから共通した処理を抜き出してさらにリファクタリングする

--- src/main/scala/jp/_5000164/backlog_bot/interfaces/Backlog.scala
+++ src/main/scala/jp/_5000164/backlog_bot/interfaces/Backlog.scala
@@ -5,7 +5,7 @@ import java.util.Date
 import com.nulabinc.backlog4j.conf.{BacklogConfigure, BacklogJpConfigure}
 import com.nulabinc.backlog4j.internal.json.activities.{IssueCommentedContent, IssueCreatedContent, IssueUpdatedContent}
 import com.nulabinc.backlog4j.{Activity, BacklogClient, BacklogClientFactory}
-import jp._5000164.backlog_bot.domain.{BuildMessage, Message}
+import jp._5000164.backlog_bot.domain.Message
 
 import scala.collection.JavaConverters._
 
@@ -19,37 +19,20 @@ class Backlog {
   def fetchMessages(lastExecutedAt: Date): List[Option[Message]] = {
     val project = client.getProject(projectKey)
     val activities = client.getProjectActivities(project.getId)
-    activities.asScala.filter(_.getCreated after lastExecutedAt).map(activity => {
-      if (activity.getType == Activity.Type.IssueCreated) {
+    activities.asScala.filter(_.getCreated after lastExecutedAt).map {
+      case activity if activity.getType == Activity.Type.IssueCreated =>
         val content = activity.getContent.asInstanceOf[IssueCreatedContent]
         val issue = client.getIssue(content.getId)
-        Some(Message(
-          BuildMessage.createPretext(projectKey, content.getKeyId, activity.getCreatedUser.getName, activity.getCreated, issue.getPriority.getName, issue.getAssignee.getName),
-          BuildMessage.createTitle(content.getSummary),
-          BuildMessage.createLink(spaceId, projectKey, content.getKeyId),
-          BuildMessage.createText(content.getDescription)
-        ))
-      } else if (activity.getType == Activity.Type.IssueUpdated) {
+        Some(Message.build(spaceId, projectKey, activity, content, issue))
+      case activity if activity.getType == Activity.Type.IssueUpdated =>
         val content = activity.getContent.asInstanceOf[IssueUpdatedContent]
         val comment = client.getIssueComment(content.getId, content.getComment.getId)
-        Some(Message(
-          BuildMessage.updatePretext(projectKey, content.getKeyId, comment.getCreatedUser.getName, comment.getCreated, comment.getChangeLog.asScala.toList),
-          BuildMessage.updateTitle(content.getSummary),
-          BuildMessage.updateLink(spaceId, projectKey, content.getKeyId, comment.getId),
-          BuildMessage.updateText(content.getComment.getContent, comment.getChangeLog.asScala.toList)
-        ))
-      } else if (activity.getType == Activity.Type.IssueCommented) {
+        Some(Message.build(spaceId, projectKey, activity, content, comment))
+      case activity if activity.getType == Activity.Type.IssueCommented =>
         val content = activity.getContent.asInstanceOf[IssueCommentedContent]
         val comment = client.getIssueComment(content.getId, content.getComment.getId)
-        Some(Message(
-          BuildMessage.commentPretext(projectKey, content.getKeyId, comment.getCreatedUser.getName, comment.getCreated),
-          BuildMessage.commentTitle(content.getSummary),
-          BuildMessage.commentLink(spaceId, projectKey, content.getKeyId, comment.getId),
-          BuildMessage.commentText(content.getComment.getContent)
-        ))
-      } else {
-        None
-      }
-    }).toList.reverse
+        Some(Message.build(spaceId, projectKey, activity, content, comment))
+      case _ => None
+    }.toList.reverse
   }
 }
