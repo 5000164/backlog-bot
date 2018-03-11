242b2ac8e80d83003820083dbbac957b772548c1
菅原 浩 <sncts35326@gmail.com>
2018-03-11 19:09:20 +0900

    refactor: 複数チャンネルにポストできるようにメッセージの構造をチャンネル名とセットにする

---
+++ src/main/scala/jp/_5000164/backlog_bot/domain/MessageBundle.scala
@@ -0,0 +1,8 @@
+package jp._5000164.backlog_bot.domain
+
+case class MessageBundle(
+                          postChannel: String,
+                          messages: List[Message]
+                        )
+
+
