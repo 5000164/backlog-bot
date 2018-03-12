c052449e3628a930645a1786fd665781c7a07bb5
菅原 浩 <sncts35326@gmail.com>
2018-03-12 16:58:35 +0900

    feat: 複数プロジェクトに対応するために取得元のプロジェクトと投稿先のチャンネルの紐付けを設定ファイルで管理する

--- src/main/scala/jp/_5000164/backlog_bot/interfaces/Application.scala
+++ src/main/scala/jp/_5000164/backlog_bot/interfaces/Application.scala
@@ -2,12 +2,15 @@ package jp._5000164.backlog_bot.interfaces
 
 import java.util.Date
 
+import jp._5000164.backlog_bot.infractructure.Settings
+
 object Application extends App {
   val backlog = new Backlog
   val slack = new Slack
 
   val lastExecutedAt = Recorder.getLastExecutedAt
-  val messageBundles = backlog.fetchMessages(lastExecutedAt)
+  val mapping = Settings.settings.mapping
+  val messageBundles = backlog.fetchMessages(lastExecutedAt, mapping)
   slack.post(messageBundles)
   Recorder.record(new Date)
 
--- src/main/scala/jp/_5000164/backlog_bot/interfaces/Backlog.scala
+++ src/main/scala/jp/_5000164/backlog_bot/interfaces/Backlog.scala
@@ -6,36 +6,37 @@ import com.nulabinc.backlog4j.conf.{BacklogConfigure, BacklogJpConfigure}
 import com.nulabinc.backlog4j.internal.json.activities.{IssueCommentedContent, IssueCreatedContent, IssueUpdatedContent}
 import com.nulabinc.backlog4j.{Activity, BacklogClient, BacklogClientFactory}
 import jp._5000164.backlog_bot.domain.{Message, MessageBundle}
+import jp._5000164.backlog_bot.infractructure.Settings
 
 import scala.collection.JavaConverters._
 
 class Backlog {
   val spaceId = sys.env("BACKLOG_SPACE_ID")
   val apiKey = sys.env("BACKLOG_API_KEY")
-  val projectKey = sys.env("BACKLOG_PROJECT_KEY")
   val configure: BacklogConfigure = new BacklogJpConfigure(spaceId).apiKey(apiKey)
   val client: BacklogClient = new BacklogClientFactory(configure).newClient()
 
-  def fetchMessages(lastExecutedAt: Date): List[MessageBundle] = {
-    val project = client.getProject(projectKey)
-    val activities = client.getProjectActivities(project.getId)
-    val messages = activities.asScala.filter(_.getCreated after lastExecutedAt).map {
-      case activity if activity.getType == Activity.Type.IssueCreated =>
-        val content = activity.getContent.asInstanceOf[IssueCreatedContent]
-        val issue = client.getIssue(content.getId)
-        Some(Message.build(spaceId, projectKey, activity, content, issue))
-      case activity if activity.getType == Activity.Type.IssueUpdated =>
-        val content = activity.getContent.asInstanceOf[IssueUpdatedContent]
-        val comment = client.getIssueComment(content.getId, content.getComment.getId)
-        Some(Message.build(spaceId, projectKey, activity, content, comment))
-      case activity if activity.getType == Activity.Type.IssueCommented =>
-        val content = activity.getContent.asInstanceOf[IssueCommentedContent]
-        val comment = client.getIssueComment(content.getId, content.getComment.getId)
-        Some(Message.build(spaceId, projectKey, activity, content, comment))
-      case _ => None
-    }.filter(_.isDefined).map(_.get).toList.reverse
-
-    val postChannel = sys.env("SLACK_POST_CHANNEL")
-    List(MessageBundle(postChannel, messages))
+  def fetchMessages(lastExecutedAt: Date, mapping: Map[Settings.settings.ProjectKey, Settings.settings.PostChannel]): List[MessageBundle] = {
+    mapping.toList.map {
+      case (projectKey, postChannel) =>
+        val project = client.getProject(projectKey)
+        val activities = client.getProjectActivities(project.getId)
+        val messages = activities.asScala.filter(_.getCreated after lastExecutedAt).map {
+          case activity if activity.getType == Activity.Type.IssueCreated =>
+            val content = activity.getContent.asInstanceOf[IssueCreatedContent]
+            val issue = client.getIssue(content.getId)
+            Some(Message.build(spaceId, projectKey, activity, content, issue))
+          case activity if activity.getType == Activity.Type.IssueUpdated =>
+            val content = activity.getContent.asInstanceOf[IssueUpdatedContent]
+            val comment = client.getIssueComment(content.getId, content.getComment.getId)
+            Some(Message.build(spaceId, projectKey, activity, content, comment))
+          case activity if activity.getType == Activity.Type.IssueCommented =>
+            val content = activity.getContent.asInstanceOf[IssueCommentedContent]
+            val comment = client.getIssueComment(content.getId, content.getComment.getId)
+            Some(Message.build(spaceId, projectKey, activity, content, comment))
+          case _ => None
+        }.filter(_.isDefined).map(_.get).toList.reverse
+        MessageBundle(postChannel, messages)
+    }
   }
 }
